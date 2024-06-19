a3cce92bfe56d5ec3e1653e2de28c750e8cf7f45
AishShrestha <shresthaayush49@gmail.com>
2024-06-19 07:10:21 +0545

    alternate way added

--- reverseCipher.py
+++ reverseCipher.py
@@ -1,9 +1,14 @@
 #Reverse Cipher
+# msg = input("Input your message: ")
+# n= len(msg)
+# reverseMsg = ''
+# i=n-1
+# while(i>=0):
+#     reverseMsg = reverseMsg +msg[i]
+#     i = i-1
+# print("Reverse cipher of message is : ", reverseMsg)
+
+#Alternate
 msg = input("Input your message: ")
-n= len(msg)
-reverseMsg = ''
-i=n-1
-while(i>=0):
-    reverseMsg = reverseMsg +msg[i]
-    i = i-1
-print("Reverse cipher of message is : ", reverseMsg)
+reverCypher = msg[::-1]
+print(reverCypher)
